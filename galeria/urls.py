9782c0b6ea965f65177d47cd5aa2a2bce198d4af
Habacuque <119132706+Habacuque-Gosch@users.noreply.github.com>
2023-12-30 08:37:54 -0300

    Delete galeria directory

--- galeria/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from galeria.views import index, imagem
-
-
-urlpatterns = [
-    path('', index, name = 'index'),
-    path('imagem/', imagem, name='imagem')
-]
--- galeria/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'galeria/index.html')
-
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
