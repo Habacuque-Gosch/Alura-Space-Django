aacd5958f1a6ba9a1a6b27b868d119e134674f4c
Habacuque-Gosch <habacuke14@gmail.com>
2024-02-14 14:56:34 -0300

    pycache

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -5,9 +5,9 @@ from django.contrib import messages
 
 
 def index(request):
-    if not request.user.is_authenticated:
-        messages.error(request, "Usuário não logado")
-        return redirect('login')
+    # if not request.user.is_authenticated:
+    #     messages.error(request, "Usuário não logado")
+    #     return redirect('login')
 
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
